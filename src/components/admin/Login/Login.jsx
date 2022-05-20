a5e58ee712119c1e303432f93a990bfe84e59fc7
Keyur-Gondaliya-403 <keyur.gondaliya@technoproject.in>
2022-05-20 15:34:33 +0530

    tost

--- src/components/admin/Login/Login.jsx
+++ src/components/admin/Login/Login.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { async } from '@firebase/util';
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { async } from "@firebase/util";
 import {
   doc,
   setDoc,
@@ -15,17 +15,17 @@ import {
   where,
   getDoc,
   isEmpty,
-} from 'firebase/firestore';
-import { db } from '../firebase/firebase';
+} from "firebase/firestore";
+import { db } from "../firebase/firebase";
 function Login() {
   const navigate = useNavigate();
 
   const auth = getAuth();
 
-  const [email, setemail] = useState('');
+  const [email, setemail] = useState("");
   const [isDB, setIsDB] = useState(false);
-  const [userID, setUserID] = useState('');
-  const [password, setpassword] = useState('');
+  const [userID, setUserID] = useState("");
+  const [password, setpassword] = useState("");
   const [disable, setDisable] = useState(false);
   const [checkVisible, setcheckVisible] = useState({
     email: false,
@@ -33,9 +33,9 @@ function Login() {
   });
   const [first, setfirst] = useState(1);
   useEffect(() => {
-    if (localStorage.getItem('forgotTest') != 'false') {
-      toast.success('Password reset mail sent successfully');
-      localStorage.setItem('forgotTest', false);
+    if (localStorage.getItem("forgotTest") != "false") {
+      toast.success("Password reset mail sent successfully");
+      localStorage.setItem("forgotTest", false);
       setfirst(first);
     }
   }, [first]);
@@ -58,34 +58,34 @@ function Login() {
     } else {
       await checkAuth(email);
 
-      localStorage.setItem('DM_Admin_ID', auth?.currentUser?.uid);
-      localStorage.setItem('DM_Admin_CURRENTUSER', auth?.currentUser);
-      localStorage.setItem('DM_Admin_EMAIL', email);
-      const docRef = doc(db, 'admin', email);
+      localStorage.setItem("DM_Admin_ID", auth?.currentUser?.uid);
+      localStorage.setItem("DM_Admin_CURRENTUSER", auth?.currentUser);
+      localStorage.setItem("DM_Admin_EMAIL", email);
+      const docRef = doc(db, "admin", auth.currentUser.uid);
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        console.log('Document data:', docSnap.data());
-        localStorage.setItem('DM_Admin_NAME', docSnap.data()?.name);
-        localStorage.setItem('DM_Admin_IMAGE', docSnap.data()?.image);
+        console.log("Document data:", docSnap.data());
+        localStorage.setItem("DM_Admin_NAME", docSnap.data()?.name);
+        localStorage.setItem("DM_Admin_IMAGE", docSnap.data()?.image);
       } else {
         // doc.data() will be undefined in this case
-        console.log('No such document!');
+        console.log("No such document!");
         const tempData = {
-          name: 'Admin',
+          name: "Admin",
           email: email,
           image:
-            'https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png',
+            "https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png",
           id: auth.currentUser.uid,
         };
-        console.log('0000', tempData);
-        await setDoc(doc(db, 'admin', email), tempData);
+        console.log("0000", tempData);
+        await setDoc(doc(db, "admin", auth.currentUser.uid), tempData);
         localStorage.setItem(
-          'DM_Admin_IMAGE',
+          "DM_Admin_IMAGE",
           `https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png`
         );
-        localStorage.setItem('DM_Admin_NAME', 'Admin');
-        console.log('created Doc');
+        localStorage.setItem("DM_Admin_NAME", "Admin");
+        console.log("created Doc");
       }
       setcheckVisible({ email: false, password: false });
     }
@@ -97,9 +97,9 @@ function Login() {
         const user = userCredential.user;
         setUserID(auth?.currentUser?.uid);
         console.log(user);
-        setemail('');
-        setpassword('');
-        navigate('/dashboard');
+        setemail("");
+        setpassword("");
+        navigate("/dashboard");
         // ...
       })
       .catch((error) => {
@@ -107,12 +107,12 @@ function Login() {
         const errorMessage = error.message;
 
         if (
-          errorMessage.search('wrong-password') === -1 ||
-          errorMessage.search('user-not-found') === -1
+          errorMessage.search("wrong-password") === -1 ||
+          errorMessage.search("user-not-found") === -1
         ) {
-          if (errorMessage.search('auth/network-request-failed') !== -1)
-            toast.error('Please, Connect with internet.');
-          else toast.error('Incorrect email or password ');
+          if (errorMessage.search("auth/network-request-failed") !== -1)
+            toast.error("Please, Connect with internet.");
+          else toast.error("Incorrect email or password ");
           setDisable(false);
         } else {
           toast.error(`Something Went Wrong`);
@@ -122,14 +122,14 @@ function Login() {
   }
 
   useEffect(() => {
-    if (localStorage.getItem('DM_Admin_ID') != null) {
-      toast.error('Already login...!');
-      navigate('/dashboard');
+    if (localStorage.getItem("DM_Admin_ID") != null) {
+      toast.error("Already login...!");
+      navigate("/dashboard");
     }
-    document.getElementById('page-loader').style.display = 'none';
+    document.getElementById("page-loader").style.display = "none";
 
-    var element = document.getElementById('page-container');
-    element.classList.add('show');
+    var element = document.getElementById("page-container");
+    element.classList.add("show");
   }, []);
 
   return (
@@ -142,7 +142,7 @@ function Login() {
         <div
           className="login-cover-image"
           style={{
-            backgroundImage: 'url(assets/img/login-bg/login-bg-17.jpg)',
+            backgroundImage: "url(assets/img/login-bg/login-bg-17.jpg)",
           }}
           data-id="login-cover-image"
         ></div>
@@ -194,13 +194,13 @@ function Login() {
                   <div className="text-danger ms-0 text-start">
                     Please enter password
                   </div>
-                ) : null}{' '}
+                ) : null}{" "}
               </div>
               {console.log(email)}
               <div className="form-group m-b-20">
                 <Link
                   to={{
-                    pathname: '/forgot-password',
+                    pathname: "/forgot-password",
                   }}
                   state={email}
                 >
@@ -215,7 +215,7 @@ function Login() {
                   disabled={disable}
                   onClick={submitHandler}
                 >
-                  {disable ? 'Processing...' : 'Sign me in'}
+                  {disable ? "Processing..." : "Sign me in"}
                 </button>
               </div>
             </form>
