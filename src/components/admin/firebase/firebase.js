5b1bb3b8e9b3faafdfd0435698e82ca141794d7f
Keyur-Gondaliya-403 <keyur.gondaliya@technoproject.in>
2022-05-26 11:44:22 +0530

    .

--- src/components/admin/firebase/firebase.js
+++ src/components/admin/firebase/firebase.js
@@ -1,25 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBjqChuUFDjUBe-7rfXGx5W-vG-bhEEY4E",
-  authDomain: "travel-app-8495d.firebaseapp.com",
-  projectId: "travel-app-8495d",
-  storageBucket: "travel-app-8495d.appspot.com",
-  messagingSenderId: "397413145576",
-  appId: "1:397413145576:web:d2fe74f0af77b9e68a625d",
-  measurementId: "G-EG9NYJWF11",
+  apiKey: "AIzaSyCzfktpXOKhoUM_XOpI-4i_CmtLgkkPXUQ",
+  authDomain: "trulipay.firebaseapp.com",
+  projectId: "trulipay",
+  storageBucket: "trulipay.appspot.com",
+  messagingSenderId: "326860589916",
+  appId: "1:326860589916:web:c40efe282d7d8a2669cf99",
+  measurementId: "G-TLW2002M2Y",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
